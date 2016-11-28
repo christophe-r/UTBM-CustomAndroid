ef5181ae42a691d9d95d76bb9acd3fdd626210b8
lo52tp <contact@christophe-ribot.fr>
2016-11-28 08:51:08 +0100

    Deleted *~ files

---
+++ utbm_customandroid/external/libmlbin_jni/src/mlbin_jni.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <mlbin.h>
+
+#include "mlbin_jni.h"
+
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinInitUsb(JNIEnv *env, jobject obj)
+{
+	return mlbin_init_usb();
+}
+
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinFreeUsb(JNIEnv *env, jobject obj)
+{
+	return mlbin_free_usb();
+}
+
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinFire(JNIEnv *env, jobject obj, jint device)
+{
+	return mlbin_fire(device);
+}
+
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinMoveDown(JNIEnv *env, jobject obj, jint device)
+{
+	return mlbin_move_down(device);
+}
+
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinMoveLeft(JNIEnv *env, jobject obj, jint device)
+{
+	return mlbin_move_left(device);
+}
+
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinMoveRight(JNIEnv *env, jobject obj, jint device)
+{
+	return mlbin_move_right(device);
+}
+
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinStop(JNIEnv *env, jobject obj)
+{
+	return mlbin_stop();
+}
+
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinCountDevices(JNIEnv *env, jobject obj)
+{
+	return mlbin_count_devices();
+}
+
---
+++ utbm_customandroid/external/libmlbin_jni/src/mlbin_jni.h
@@ -0,0 +1,15 @@
+#include <jni.h>
+
+#ifndef _MLBIN_JNI_H_
+#define _MLBIN_JNI_H_
+
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinInitUsb(JNIEnv *env, jobject obj);
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinFreeUsb(JNIEnv *env, jobject obj);
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinFire(JNIEnv *env, jobject obj, jint device);
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinMoveDown(JNIEnv *env, jobject obj, jint device);
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinMoveLeft(JNIEnv *env, jobject obj, jint device);
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinMoveRight(JNIEnv *env, jobject obj, jint device);
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinStop(JNIEnv *env, jobject obj);
+JNIEXPORT jint JNICALL Java_com_android_utbm_customandroid_MissileLauncherInterface_MlbinCountDevices(JNIEnv *env, jobject obj);
+
+#endif
