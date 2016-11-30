1dd7441716a80d06daf71e8833f51ffc46efcb4e
Christophe <contact@christophe-ribot.fr>
2016-11-30 20:39:15 +0100

    Added WatchActivity in Media Center App

--- MediaCenterApp/app/src/main/java/fr/utbm/lo52/CustomAndroid/mediacenter/View/VideoCardsViewHolder.java
+++ MediaCenterApp/app/src/main/java/fr/utbm/lo52/CustomAndroid/mediacenter/View/VideoCardsViewHolder.java
@@ -1,16 +1,19 @@
 package fr.utbm.lo52.CustomAndroid.mediacenter.View;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
 
+import fr.utbm.lo52.CustomAndroid.mediacenter.Controller.WatchActivity;
 import fr.utbm.lo52.CustomAndroid.mediacenter.Model.Video;
 import fr.utbm.lo52.CustomAndroid.mediacenter.R;
 
@@ -23,6 +26,7 @@ public class VideoCardsViewHolder extends RecyclerView.ViewHolder{
     private TextView videoTitleView;
     private TextView videoYearView;
     private ImageView videoImageView;
+    private Button videoButtonWatch;
 
     //itemView est la vue correspondante à 1 cellule
     public VideoCardsViewHolder(View itemView) {
@@ -33,6 +37,11 @@ public class VideoCardsViewHolder extends RecyclerView.ViewHolder{
         videoTitleView = (TextView) itemView.findViewById(R.id.video_title);
         videoYearView = (TextView) itemView.findViewById(R.id.video_year);
         videoImageView = (ImageView) itemView.findViewById(R.id.video_image);
+        videoButtonWatch = (Button) itemView.findViewById(R.id.video_button_watch);
+
+
+
+
     }
 
     //puis ajouter une fonction pour remplir la cellule en fonction d'un Video
@@ -52,6 +61,22 @@ public class VideoCardsViewHolder extends RecyclerView.ViewHolder{
         } else {
             Log.e("File Not Found", imgFile.getPath());
         }
+
+        videoButtonWatch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), WatchActivity.class);
+                intent.putExtra("VIDEO_TITLE", "My video");
+                intent.putExtra("VIDEO_PATH", "");
+                v.getContext().startActivity(intent);
+
+            }
+
+        });
+
+
+
+
 
     }
 }
